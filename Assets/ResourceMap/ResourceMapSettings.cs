a77e86736c6b330fd6b2ac4e78ad0d1d53270777
BrionStoutarm <macksmith675@gmail.com>
2021-04-26 18:42:36 -0700

    Resource Start
    Start to resource map code

---
+++ Assets/ResourceMap/ResourceMapSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ResourceMapSettings : UpdatableData
+{
+    //Biome biome;
+
+    public Resource[] availableResources;
+
+
+}
