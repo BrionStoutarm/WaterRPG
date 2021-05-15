422cba447145a21a62a6bd02bb9dfd9387f8c624
mikeFeliz <mike.c.feliz@gmail.com>
2021-05-14 18:11:36 -0700

    Add FollowCamera
    Add script to have the camera follow an arbitrary game object. Currently set to the PlayerBoat for the demo by default.

---
+++ Assets/Camera/FollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject m_toFollow;
+    public float m_speed = 1.00f;
+    public Vector3 m_offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_offset = transform.position; //hacky
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_toFollow)
+        {
+            Vector3 target = m_toFollow.transform.position;
+            target += m_offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, target , m_speed);
+            transform.position = smoothedPosition;
+        }
+        
+    }
+}
