0bdef510f6a477761e214add004c5a607e3b6ee9
mikeFeliz <mike.c.feliz@gmail.com>
2021-05-21 22:40:17 -0700

    Initial Checkin for GoTo AI
    AI boat control to move towards an objective. Needs logic for evaluating tacking vs. locally greedy heading.

--- Assets/GameManager/AIBoatControl.cs
+++ Assets/GameManager/AIBoatControl.cs
@@ -6,26 +6,28 @@ using Move = System.Tuple<BoatMovement.RotationSetting, BoatMovement.MovementSet
 
 public class AIBoatControl : MonoBehaviour
 {
-    
+
 
     public Vector3 m_target;
+    public float m_gotoThreshold = 2f;
     private BoatMovement m_boat;
     private GameManager m_gameManager;
-    
+
     public enum Mode
     {
         RANDOM,
-        MAX_SPEED
+        MAX_SPEED,
+        GO_TO
     }
     public Mode m_mode;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         m_gameManager = GameManager.Get();
-        m_mode = Mode.MAX_SPEED;
+        m_mode = Mode.GO_TO;
     }
 
     // Update is called once per frame
@@ -45,6 +47,9 @@ public class AIBoatControl : MonoBehaviour
                 case (Mode.MAX_SPEED):
                     MoveMaxSpeed();
                     break;
+                case (Mode.GO_TO):
+                    MoveGoTo();
+                    break;
                 default:
                     break;
             }
@@ -55,12 +60,17 @@ public class AIBoatControl : MonoBehaviour
         m_boat = boat;
     }
 
+    public void SetTarget(Vector3 target)
+    {
+        m_target = target;
+    }
+
     public void MoveRandomly()
     {
         float turnRand = UnityEngine.Random.value;
-        if(turnRand > .9)
+        if (turnRand > .9)
         {
-            if(turnRand > .96)
+            if (turnRand > .96)
             {
                 m_boat.LiveRotationSetting(BoatMovement.RotationSetting.FORWARD);
             }
@@ -74,7 +84,7 @@ public class AIBoatControl : MonoBehaviour
             }
         }
         float speedRand = UnityEngine.Random.value;
-        if(speedRand > .95)
+        if (speedRand > .95)
         {
             if (speedRand > .985)
             {
@@ -93,7 +103,7 @@ public class AIBoatControl : MonoBehaviour
         ret.Add(new Move(BoatMovement.RotationSetting.FORWARD, curMov));
         ret.Add(new Move(BoatMovement.RotationSetting.LEFT, curMov));
         ret.Add(new Move(BoatMovement.RotationSetting.RIGHT, curMov));
-        if (curMov > 0)
+        if (curMov > BoatMovement.MovementSetting.FULL_STOP) //Exclude oarsback to avoid cases where the AI won't turn
         {
             ret.Add(new Move(BoatMovement.RotationSetting.FORWARD, curMov - 1));
             ret.Add(new Move(BoatMovement.RotationSetting.LEFT, curMov - 1));
@@ -109,7 +119,7 @@ public class AIBoatControl : MonoBehaviour
     }
 
     public float MaxSpeedScore(Move m)
-    {    
+    {
         Vector3 predictedLocation = m_boat.LivePredictLocation(m.Item1, m.Item2);
         float score = Vector3.Distance(m_boat.transform.position, predictedLocation);
         //Debug.Log(string.Format("{0} {1} {2}", BoatMovement.RotationSettingToString(m.Item1), BoatMovement.MovementSettingToString(m.Item2), score));
@@ -122,6 +132,39 @@ public class AIBoatControl : MonoBehaviour
         ExecuteMove(move);
     }
 
+    public float GoToScore(Move m)
+    {
+        Vector3 predictedLocation = m_boat.LivePredictLocation(m.Item1, m.Item2);
+        float score = 1/(1 + Vector3.Distance(m_target, predictedLocation));
+        Debug.Log(string.Format("{0} {1} {2} {3}", BoatMovement.RotationSettingToString(m.Item1), BoatMovement.MovementSettingToString(m.Item2), Vector3.Distance(m_target, predictedLocation), score));
+        return score;
+    }
+
+    public void MoveGoTo()
+    {
+        var moves = GenerateMoves();
+        LegalizeGoTo(ref moves);
+        var move = ChooseMove(moves, GoToScore);
+        ExecuteMove(move);
+    }
+
+    private bool isFullStop(Move m)
+    {
+        if(m.Item2 == BoatMovement.MovementSetting.FULL_STOP)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void LegalizeGoTo(ref List<Move> moveList)
+    {
+        if (Vector3.Distance(m_boat.transform.position, m_target) > m_gotoThreshold);
+        {
+            moveList.RemoveAll(isFullStop);
+        }
+    }
+    
     public void ExecuteMove(Move toMove)
     {
         m_boat.m_rotationSetting = toMove.Item1;
@@ -132,87 +175,19 @@ public class AIBoatControl : MonoBehaviour
     {
         float bestScore = 0;
         Move bestMove = moveList[0];
-        //string logStr = "";
+        string logStr = "";
         foreach(Move m in moveList)
         {
             float score = scoreMove(m);
-            //logStr += string.Format("{0} {1} {2}   ", BoatMovement.RotationSettingToString(m.Item1), BoatMovement.MovementSettingToString(m.Item2), score);
+            logStr += string.Format("{0} {1} {2}   ", BoatMovement.RotationSettingToString(m.Item1), BoatMovement.MovementSettingToString(m.Item2), score);
             if (score > bestScore)
             {
                 bestMove = m;
                 bestScore = score;
             }
         }
-        //Debug.Log(logStr);
+        Debug.Log(logStr);
         //Debug.Log(string.Format("{0} {1} {2}", BoatMovement.RotationSettingToString(bestMove.Item1), BoatMovement.MovementSettingToString(bestMove.Item2), bestScore));
         return bestMove;
     }
 }
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerBoatControl : MonoBehaviour
-{
-    public KeyCode m_speedUp = KeyCode.W;
-    public KeyCode m_speedDown = KeyCode.S;
-    public KeyCode m_turnLeft = KeyCode.A;
-    public KeyCode m_turnRight = KeyCode.D;
-    private BoatMovement m_boat;
-    private GameManager m_gameManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_gameManager = GameManager.Get();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_boat)
-        {
-            if (Input.GetKeyDown(m_speedUp))
-            {
-                m_boat.IncreaseMovementSetting();
-            }
-
-            if (Input.GetKeyDown(m_speedDown))
-            {
-                m_boat.DecreaseMovementSetting();
-            }
-
-            if (checkTurnKey(m_turnLeft) == checkTurnKey(m_turnRight))
-            {
-                m_boat.TurnForward();
-            }
-            else if (checkTurnKey(m_turnRight))
-            {
-                m_boat.TurnRight();
-            }
-            else
-            {
-                m_boat.TurnLeft();
-            }
-        }
-    }
-
-    private bool checkTurnKey(KeyCode c)
-    {
-        if (m_gameManager.Paused())
-        {
-            return Input.GetKeyDown(c);
-        }
-        else
-        {
-            return Input.GetKey(c);
-        }
-    }
-
-    public void SetBoat(BoatMovement boat)
-    {
-        m_boat = boat;
-    }
-}
-*/
